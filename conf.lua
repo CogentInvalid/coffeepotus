f56d354fdfef9286f829bb8bcc8b66cad473de2a
CogentInvalid <pchipman@email.arizona.edu>
2015-11-22 11:01:17 -0700

    set window title/icon

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
-	t.window.title = "Untitled"
+	t.window.title = "More Coffee, Mr. President?"
+	t.window.icon = "icon.png"
 	t.window.width = 1000
     t.window.height = 600
 	t.window.resizable = true
